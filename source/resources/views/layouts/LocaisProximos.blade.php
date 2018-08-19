b9d81a640c449d92ab62f54c4067c7db691c8408
djeymisson <djeymisson@gmail.com>
2018-08-19 00:17:31 -0300

    Adicionada API de localização por IP do cliente, e função de recomendação de anuncios próximos na Home.

--- source/resources/views/layouts/LocaisProximos.blade.php
+++ source/resources/views/layouts/LocaisProximos.blade.php
@@ -1,18 +1,20 @@
 <body><!--locais próximos-->
+  <?php $anunciosProx = \App\Http\Controllers\AnuncioController::getAnunciosProximos() ?>
+  @if (!empty($anunciosProx['anuncios']))
     <div class="grey lighten-4 hide-on-med-and-down">
-        <span class="grey-text text-lighten-4">
-            <hr/>
-        </span>
+      <span class="grey-text text-lighten-4">
+          <hr/>
+      </span>
         <div class="row container">
             <h4>
                 <span class="left grey-text text-darken-3">
-                    &nbsp;Lugares próximos de você
+                    &nbsp;Lugares próximos de {{ $anunciosProx['cidade'] }} - {{ $anunciosProx['estado'] }}
                 </span>
             </h4>
         </div>
         <!--esse código todo será gerado automaticamente-->
         <div class="row container">
-          @foreach ($anuncios as $anuncio)
+          @foreach ($anunciosProx['anuncios'] as $anuncio)
             <?php $adData = \App\Http\Controllers\AnuncioController::getDadosAnuncio($anuncio->id) ?>
 
             <div class="col s06 m3 l3 xl3">
@@ -54,209 +56,5 @@
             <hr/>
         </span>
     </div>
-
-    <div class="orange accent-4 hide-on-large-only">
-        <span class="grey-text text-lighten-4">
-            <hr/>
-        </span>
-        <div class="row container">
-            <h4>
-                <span class="left white-text text-darken-3">
-                    &nbsp;Perto de você
-                </span>
-            </h4>
-        </div>
-
-        <div class="carousel" id="carouselHome">
-            <div class="carousel-item black-text" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item" style="height:400px !important; width:300px !important;">
-                <div class="card">
-                    <div class="card-image">
-                        <img class="activator" src="https://i.imgur.com/vF4bF8z.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Nome da oferta
-                            <i class="material-icons right">more_vert</i>
-                        </span>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            <i class="material-icons right">close</i>
-                        </span>
-                        <p>
-                            Resumo curto da oferta, texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            texto texto texto texto texto.
-                        </p>
-                        <p>
-                            <a href="#" class=" green-text text-darken-3">
-                                <b>Visitar</b>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <span class="grey-text text-lighten-4">
-            <hr/>
-        </span>
-    </div>
+  @endif
 </body>
