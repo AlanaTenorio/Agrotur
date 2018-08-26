9cd9b85ee18807827b310e8535ed32b6c46a53fa
FELIPE-DESKTOP\felip <felipeliborio@outlook.com>
2018-08-25 22:52:13 -0300

    Id do anúncio adicionado a tabela da mensagem

--- source/app/Message.php
+++ source/app/Message.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Message extends Model
 {
-    protected $fillable = ['text', 'sender_id', 'recipient_id', 'b_read'];
+    protected $fillable = ['text', 'ad_id', 'sender_id', 'recipient_id', 'b_read'];
 
     public static $rules = [
         'text' => 'required',
+        'ad_id' => 'required|integer',
         'sender_id' => 'required|integer',
         'recipient_id' => 'required|integer',
         'b_read' => 'boolean',
@@ -18,6 +19,8 @@ class Message extends Model
 
     public static $messages = [
         'text.required' => '"text" field can\'t be left empty!',
+        'ad_id.required' => '"ad_id" field can\'t be left empty!',
+        'ad_id.integer' => '"ad_id" value must be integer!',
         'sender_id.required' => '"sender_id" field can\'t be left empty!',
         'sender_id.integer' => '"sender_id" value must be integer!',
         'recipient_id.required' => '"recipient_id" field can\'t be left empty!',
@@ -25,6 +28,10 @@ class Message extends Model
         'b_read.boolean' => '"b_read" value must be boolean!',
     ];
   
+    public function ad_id(){
+        return $this->belongsToOne('app\Anuncio');
+    }
+
     public function sender_id(){
         return $this->belongsToOne('app\Cliente');
     }
--- source/database/migrations/2018_08_25_231637_create_messages_table.php
+++ source/database/migrations/2018_08_25_231637_create_messages_table.php
@@ -17,9 +17,11 @@ class CreateMessagesTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->text('text');
+            $table->integer('ad_id')->unsigned();
             $table->integer('sender_id')->unsigned();
             $table->integer('recipient_id')->unsigned();
             $table->boolean('b_read')->default(false);
+            $table->foreign('ad_id')->references('id')->on('anuncios');
             $table->foreign('sender_id')->references('id')->on('clientes');
             $table->foreign('recipient_id')->references('id')->on('clientes');
         });
